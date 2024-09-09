b2a932d09702d660b6a32f84d2cf242dc4d04cfb
kan2269 <watari.tls@gmail.com>
2024-09-09 21:55:24 +0900

    簡単なあいさつ

---
+++ resources/views/comments/afternoon.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>単純なあいさつ</title>
+</head>
+
+<body>
+    <h1>昼のあいさつ</h1>
+    <h2>こんにちは</h2>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/comments/mornig', function () {
+    return view('comments.mornig');
+});
+
+Route::get('/comments/afternoon', function () {
+    return view('comments.afternoon');
+});
+
+Route::get('/comments/evening', function () {
+    return view('comments.evening');
+});
+
+Route::get('/comments/night', function () {
+    return view('comments.night');
+});
